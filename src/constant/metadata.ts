442b587381390e00ad590133f17c820cc93d8b62
huyenbtk <khanhhuyensthp@gmail.com>
2025-09-17 18:05:19 +0700

    update: modify site metadata and enhance UI components with improved scrolling and loading states

--- src/constant/metadata.ts
+++ src/constant/metadata.ts
@@ -1,8 +1,8 @@
 import THUMBNAIL_IMG from 'public/thumbnail.png';
 
 export const SITE_URL = 'http://localhost:3000/';
-export const SITE_TITLE = 'Next Template';
-export const SITE_DESCRIPTION = 'Next Template Description';
+export const SITE_TITLE = 'OVIRA';
+export const SITE_DESCRIPTION = 'OVIRA Description';
 export const KEYWORD = ['A-Star Group', 'A-Star', 'Group'];
 
 export const THUMBNAIL = THUMBNAIL_IMG;
